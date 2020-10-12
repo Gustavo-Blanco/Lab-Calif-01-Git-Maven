c4443b14f6160e1448f81626c90209933ceb1941
yeridi <yeridicrhis2002@gmail.com>
2020-10-12 17:00:08 -0500

    Mensaje de impresion

--- src/com/tecsup/lab01/view/UserView.java
+++ src/com/tecsup/lab01/view/UserView.java
@@ -1,11 +1,14 @@
 package com.tecsup.lab01.view;
+import com.tecsup.lab01.controller.*;
+import com.tecsup.lab01.model.*;
 
 public class UserView {
-	String nombre ="Yeridi Crhis";
-	String apellido="Huaman Taquire";
-	int edad=23;
-	
-	public void imprimir() {
-		System.out.println("Hola "+ nombre + apellido + "tienes "+ edad + "años");
+	public static void main(String[] args) {
+		
+		User u = new Userr("Tomas","Espinoza Rojaz"+16);
+		UserController b = new UserController(u);
+		c=b.imprimir();
+		System.out.println("Mi nombre y edad es : "+c);
 	}
+	
 }
