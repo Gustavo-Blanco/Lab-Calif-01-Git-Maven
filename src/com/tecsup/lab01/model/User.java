cdceff2995d5189c8b02a69d8e6376555443996b
Gustavo <gblanco@gmail.com>
2020-10-12 16:26:58 -0500

    Commit branch gustavo

--- src/com/tecsup/lab01/model/User.java
+++ src/com/tecsup/lab01/model/User.java
@@ -1,5 +1,38 @@
 package com.tecsup.lab01.model;
 
 public class User {
+	private String nombre;
+	private String apellidoPaterno;
+	private int edad;
+	
+	public User(String nombre, String apellidoPaterno, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellidoPaterno = apellidoPaterno;
+		this.edad = edad;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 }
