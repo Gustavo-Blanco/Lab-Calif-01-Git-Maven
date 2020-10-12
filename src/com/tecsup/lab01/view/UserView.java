4bc10374f55f31bf539d94e658adab0e2b91870f
yeridi <yeridicrhis2002@gmail.com>
2020-10-12 16:56:41 -0500

    Impresion de Nombre

--- src/com/tecsup/lab01/view/UserView.java
+++ src/com/tecsup/lab01/view/UserView.java
@@ -1,5 +1,11 @@
 package com.tecsup.lab01.view;
 
 public class UserView {
-
+	String nombre ="Yeridi Crhis";
+	String apellido="Huaman Taquire";
+	int edad=23;
+	
+	public void imprimir() {
+		System.out.println("Hola "+ nombre + apellido + "tienes "+ edad + "años");
+	}
 }
