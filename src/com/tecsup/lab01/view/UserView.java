4c3ce8885c9e05bca1051fbc7c7629c22fc2f25f
yeridi <yeridicrhis2002@gmail.com>
2020-10-12 17:11:31 -0500

    Impresion

--- src/com/tecsup/lab01/view/UserView.java
+++ src/com/tecsup/lab01/view/UserView.java
@@ -5,10 +5,9 @@ import com.tecsup.lab01.model.*;
 public class UserView {
 	public static void main(String[] args) {
 		
-		User u = new Userr("Tomas","Espinoza Rojaz"+16);
+		User u = new Userr("Tomas","Espinoza Rojaz",+16);
 		UserController b = new UserController(u);
-		c=b.imprimir();
-		System.out.println("Mi nombre: "+c);
+		System.out.println("Mi nombre: "+b.imprimir());
 	}
 	
 }
